735bf19e8d73423fb72aa3e7e8740f413cb8512f
axel3585 <damiancito399@gmail.com>
2023-11-14 21:26:52 -0300

    arreglo

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 function ItemListContainer() {
   return (
-    <section className="item-list-container">
+    <section>
       <h2>Productos Destacados</h2>
+     
+      <div className="category">
+        <h3>Celulares Nuevos</h3>
+        
+      </div>
+      <div className="category">
+        <h3>Celulares Reacondicionados</h3>
+       
+      </div>
     </section>
   );
 }
